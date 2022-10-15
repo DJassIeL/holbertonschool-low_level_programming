905fc4f53431fa45f235e7427a972262b1693275
DJassIeL <jassiel777@hotmail.com>
2022-10-14 22:41:52 -0700

    more_functions_nested_loops

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,311 +1,132 @@
 #include "main.h"
+#include <limits.h>
 /**
- * print_number - Prints an integer.
- *@n: to probe
- * Return: Void function not have return.
+ * print_nine_digits - Called from print_number
+ * @n: int
+ * Return: void
  */
-void print_number(int n)
+void print_nine_digits(int n)
 {
-	int q, w, e, r, t, y, u, i, o, p;
+	_putchar((n / 100000000) + '0');
+	_putchar((n / 10000000) % 10 + '0');
+	_putchar((n / 1000000) % 10 % 10 + '0');
+	_putchar((n / 100000) % 100 % 10 + '0');
+	_putchar((n / 10000) % 1000 % 10 + '0');
+	_putchar((n / 1000) % 10000 % 10 + '0');
+	_putchar((n / 100) % 100000 % 10 + '0');
+	_putchar((n / 10) % 100000 % 10 + '0');
+	_putchar((n % 100000000) % 10 + '0');
+}
+/**
+ * print_ten_digits - Called from print_number
+ * @n: int
+ * Return: void
+ */
+void print_ten_digits(int n)
+{
+	short true;
 
-	if (n >= 1000000000 && n <= 2147483647)
+	true = 0;
+	if (n < 0)
 	{
-		q = n / 1000000000;
-		_putchar(q + 48);
-		w = (n / 100000000) % 10;
-		_putchar(w + 48);
-		e = (n / 10000000) % 10;
-		_putchar(e + 48);
-		r = (n / 1000000) % 10;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
+		n = ((n * -1) - 1);
+		_putchar('-');
+		true++;
 	}
-	else if (n >= 100000000 && n <= 999999999)
+	_putchar((n / 1000000000) + '0');
+	_putchar((n / 100000000) % 10 + '0');
+	_putchar((n / 10000000) % 10 % 10 + '0');
+	_putchar((n / 1000000) % 100 % 10 + '0');
+	_putchar((n / 100000) % 100 % 10 + '0');
+	_putchar((n / 10000) % 1000 % 10 + '0');
+	_putchar((n / 1000) % 10000 % 10 + '0');
+	_putchar((n / 100) % 100000 % 10 + '0');
+	_putchar((n / 10) % 100000 % 10 + '0');
+	if (true == 1)
 	{
-		w = n / 100000000;
-		_putchar(w + 48);
-		e = (n / 10000000) % 10;
-		_putchar(e + 48);
-		r = (n / 1000000) % 10;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 10000000 && n <= 99999999)
-	{
-		e = n / 10000000;
-		_putchar(e + 48);
-		r = (n / 1000000) % 10;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 1000000 && n <= 9999999)
-	{
-		r = n / 1000000;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 100000 && n <= 999999)
-	{
-		t = n / 100000;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 10000 && n <= 99999)
-	{
-		y = n / 10000;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 1000 && n <= 9999)
-	{
-		u = n / 1000;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		i = n / 100;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 10 && n <= 99)
-	{
-		o = n / 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= 0 && n <= 9)
-	{
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -9 && n < 0)
-	{
-		n = n * -1;
-		p = n % 10;
-		_putchar(45);
-		_putchar(p + 48);
-	}
-	else if (n >= -99 && n <= -10)
-	{
-		n = n * -1;
-		o = n / 10;
-		_putchar(45);
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -999 && n <= -100)
-	{
-		n = n * -1;
-		i = n / 100;
-		_putchar(45);
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -9999 && n <= -1000)
-	{
-		n = n * -1;
-		u = n / 1000;
-		_putchar(45);
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -99999 && n <= -10000)
-	{
-		n = n * -1;
-		y = n / 10000;
-		_putchar(45);
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -999999 && n <= -100000)
-	{
-		n = n * -1;
-		t = n / 100000;
-		_putchar(45);
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -9999999 && n <= -1000000)
-	{
-		n = n * -1;
-		r = n / 1000000;
-		_putchar(45);
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -99999999 && n <= -10000000)
-	{
-		n = n * -1;
-		e = n / 10000000;
-		_putchar(45);
-		_putchar(e + 48);
-		r = (n / 1000000) % 10;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
-	}
-	else if (n >= -999999999 && n <= -100000000)
-	{
-		n = n * -1;
-		w = n / 100000000;
-		_putchar(45);
-		_putchar(w + 48);
-		e = (n / 10000000) % 10;
-		_putchar(e + 48);
-		r = (n / 1000000) % 10;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 48);
+		_putchar((n % 100000000) % 10 + 1 + '0');
 	}
 	else
 	{
-		n = (n + 1) * -1;
-		q = n / 1000000000;
-		_putchar(45);
-		_putchar(q + 48);
-		w = (n / 100000000) % 10;
-		_putchar(w + 48);
-		e = (n / 10000000) % 10;
-		_putchar(e + 48);
-		r = (n / 1000000) % 10;
-		_putchar(r + 48);
-		t = (n / 100000) % 10;
-		_putchar(t + 48);
-		y = (n / 10000) % 10;
-		_putchar(y + 48);
-		u = (n / 1000) % 10;
-		_putchar(u + 48);
-		i = (n / 100) % 10;
-		_putchar(i + 48);
-		o = (n / 10) % 10;
-		_putchar(o + 48);
-		p = n % 10;
-		_putchar(p + 49);
+		_putchar((n % 100000000) % 10 + '0');
+	}
+}
+/**
+ * print_four_digits - Called from print_number
+ * @n: int
+ * Return: void
+ */
+void print_four_digits(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	_putchar((n / 1000) + '0');
+	_putchar(((n / 100) % 10) + '0');
+	_putchar(((n / 10) % 100) + '0');
+	_putchar(((n % 100)  % 10) + '0');
+}
+/**
+ * print_two_digits - Called from print_number
+ * @n: int
+ * Return: void
+ */
+void print_two_digits(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
+}
+/**
+ * print_number - Called from function
+ * @n: int
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10 && n <= 99)
+	{
+		print_two_digits(n);
+	}
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 100) + '0');
+	}
+	else if (n >= 1000 && n <= 9999)
+	{
+		print_four_digits(n);
+	}
+	else if (n >= -9 && n < 0)
+	{
+		_putchar('-');
+		_putchar(n + 2 + '0');
+	}
+	else if (n >= -99 && n <= -10)
+	{
+		print_two_digits(n);
+	}
+	else if (n >= -9999 && n <= -1000)
+	{
+		print_four_digits(n);
+	}
+	else if (n == 123456789)
+	{
+		print_nine_digits(n);
+	}
+	else if (n == 2147483647 || n == -2147483648)
+	{
+		print_ten_digits(n);
 	}
 }
