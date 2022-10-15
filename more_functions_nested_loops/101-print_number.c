08bea75a1f563ba051e7d11cd8b58bd49f1f96d5
DJassIeL <jassiel777@hotmail.com>
2022-10-14 22:19:37 -0700

    more_functions_nested_loops

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,79 @@
-# make -f 100 Makefile executable deletes and recompilation 
+#include "main.h"
+#include <stdlib.h>
 
-CC = gcc
-OBJ = $(SRC:.c=.o)
-NAME = holberton
-SRC = main.c holberton.c
-CFLAGS = -Wall -Werror -Wextra -pedantic
 
-.PHONY: all clean oclean fclean re
+/**
+ *print_numbers - prints number n (from -99999 to 99999)
+ *@n: number to print
+ *return: always 0
+ */
 
-clean:
-	$(RM) *~ $(NAME)
+void print_numbers(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	if (n >= 10000 || n <= -10000)
+	{
+		int a;
+		int b;
+		int c;
+		int d;
+		int e;
 
-all: m.h $(OBJ)
-	$(CC) $(OBJ) -o $(NAME)
+		a = abs(n) / 10000;
+		b = (abs(n) % 10000) / 1000;
+		c = (abs(n) % 1000) / 100;
+		d = (abs(n) % 100) / 10;
+		e = abs(n) % 10;
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar(e + '0');
+	}
+	else if (n >= 1000 || n <= -1000)
+	{
+		int a;
+		int b;
+		int c;
+		int d;
 
-fclean:clean oclean
+		a = abs(n) / 1000;
+		b = (abs(n) % 1000) / 100;
+		c = (abs(n) % 100) / 10;
+		d = abs(n) % 100;
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(c + '0');
+		_putchar(d + '0');
+	}
+	else if (n >= 100 || n <= -100)
+	{
+		int a;
+		int b;
+		int c;
 
-oclean:
-	$(RM) $(OBJ)
+		a = abs(n) / 100;
+		b = (abs(n) % 100) / 10;
+		c = abs(n) % 10;
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(c + '0');
+	}
+	else if (n >= 10 || n <= -10)
+	{
+		int a;
+		int b;
 
-re: oclean all
+		a = abs(n) / 10;
+		b = abs(n) % 10;
+		_putchar(a + '0');
+		_putchar(b + '0');
+	}
+	else
+	{
+		_putchar(abs(n) + '0');
+	}
+}
